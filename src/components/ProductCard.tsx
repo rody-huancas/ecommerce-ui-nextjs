4d70252b6e74baf0bff602eec0abf53e72b2ada4
Rody Huancas <rodyhuancas.04@gmail.com>
2025-08-14 22:21:58 -0500

    Feat: Improvement of the ProductCard component with product details, size and color selection, and add to cart button

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,14 @@
-import { ProductType } from "@/types";
-import Image from "next/image";
+"use client"
+
 import Link from "next/link";
+import Image from "next/image";
+import { ProductType } from "@/types";
+import { ShoppingCart } from "lucide-react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
+      {/* IMAGE */}
       <Link href={`/products/${product.id}`}>
         <div className="relative aspect-[2/3]">
           <Image
@@ -15,6 +19,54 @@ const ProductCard = ({ product }: { product: ProductType }) => {
           />
         </div>
       </Link>
+
+      {/* PRODUCT DETAIL */}
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font-medium">{product.name}</h1>
+        <p className="text-sm text-gray-500">{product.shortDescription}</p>
+
+        {/* PRODUCT TYPES */}
+        <div className="flex items-center gap-4 text-xs">
+          {/* SIZES */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Tamaños</span>
+            <select
+              name="size"
+              id="size"
+              className="ring ring-gray-300 rounded-md px-2 py-1"
+            >
+              {product.sizes.map((size) => (
+                <option key={size} value={size}>
+                  {size.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+          {/* COLORS */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Color</span>
+            <div className="flex items-center gap-2">
+              {product.colors.map((color) => (
+                <div key={color}>
+                  <div
+                    className="w-[14px] h-[14px] rounded-full"
+                    style={{ backgroundColor: color }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* PRICE AND ADD TO CART BUTTON */}
+        <div className="flex items-center justify-between">
+          <p className="font-medium">{product.price.toFixed(2)}</p>
+          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+            <ShoppingCart className="w-4 h-4" />
+            Agregar al carrito
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
