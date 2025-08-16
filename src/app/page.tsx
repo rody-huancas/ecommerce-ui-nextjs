55a818de32c9da7be07e42cc21b71f2975395732
Rody Huancas <rodyhuancas.04@gmail.com>
2025-08-15 23:29:15 -0500

    Feat: Modify the HomePage component to dynamically obtain the product category from the search parameters and pass it to the ProductList component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import ProductList from "@/components/ProductList"
 import Image from "next/image"
 
-const HomePage = () => {
+const HomePage = async({ searchParams }: { searchParams: Promise<{ category: string }> }) => {
+
+  const category = (await searchParams).category;
+
   return (
     <div className=''>
       <div className="relative aspect-[3/1] mb-12">
         <Image src="/featured.png" alt="Producto destacado" fill />
       </div>
 
-      <ProductList category="" params="homepage" />
+      <ProductList category={category} params="homepage" />
     </div>
   )
 }
