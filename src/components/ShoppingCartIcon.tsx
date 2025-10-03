5df0409744ef40ecb06ba94349250c1519b64b0f
Rody Huancas <rodyhuancas.04@gmail.com>
2025-10-03 03:07:43 -0500

    Feat: add quantity for icon cart

--- src/components/ShoppingCartIcon.tsx
+++ src/components/ShoppingCartIcon.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
+import useCartStore from "@/store/cartStore";
+import { ShoppingCart } from "lucide-react";
 
 const ShoppingCartIcon = () => {
+  const { cart, hasHydrated } = useCartStore();
+
+  if (!hasHydrated) return null;
+  
   return (
     <Link href="/cart" className="relative">
       <ShoppingCart className="w-4 h-4 text-gray-600" />
       <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">
-        0
+        {cart.reduce((acc, item) => acc + item.quantity, 0)}
       </span>
     </Link>
   );
